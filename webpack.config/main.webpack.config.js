5c30965a00846296ffe3bf4a9466164b031d5c09
RedHornetStudio <51362978+RedHornetStudio@users.noreply.github.com>
2020-12-18 22:29:25 +0200

    project setup

---
+++ webpack.config/main.webpack.config.js
@@ -0,0 +1,93 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: './src/main/assets/js/index.js',
+  output: {
+    path: path.resolve(__dirname, '../dist'),
+    filename: 'assets/js/bundle.js',
+    publicPath: ''
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, '../dist'),
+    publicPath: ''
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+        options: {
+          attributes: {
+            list: [
+              // All default supported tags and attributes
+              '...',
+              {
+                tag: 'link',
+                attribute: 'href',
+                type: 'src',
+                filter: (tag, attribute, attributes, resourcePath) => {
+                  if (attributes.rel && attributes.rel.trim().toLowerCase() === 'icon') {
+                    return true;
+                  }
+                  return false;
+                }
+              }
+            ]
+          }
+        }
+      },
+      {
+        test: /\.scss$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: '../../',
+            },
+          }, 
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        loader: 'file-loader',
+        options: {
+          name: 'assets/img/[contenthash].[ext]'
+        }
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        loader: 'file-loader',
+        options: {
+          name: 'assets/fonts/[contenthash].[ext]'
+        }
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin({
+      cleanOnceBeforeBuildPatterns: ['./assets/*', './index.html']
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'src/main/index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'assets/css/style.css'
+    })
+  ]
+};
